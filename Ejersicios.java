4d6e254a09c31e547b48e7b091aada29cd7d5adf
marcobia09 <kr8048984@gmail.com>
2025-08-14 01:55:50 +0000

    no hubo cambios

--- Ejersicios.java
+++ Ejersicios.java
@@ -1,6 +1,24 @@
 public class Biblioteca {
     public static void main(String[] args) {
         String libros[] = new String[10]:
-        System.out.printIn(libros[10]):
+        Libros[0] = "El principito";
+        Libros[1] = "El señor de los anillos";
+        Libros[2] = "La biblia";
+        Libros[3] = "El cojote";
+        Libros[4] = "La divina comedia";
+        Libros[5] = "El principito";
+        Libros[6] = "Harry potter";
+        Libros[7] = "El cojote";
+        Libros[8] = "La biblia";
+        Libros[9] = "La divina comedia";
+
+        for (int i = 
+        < Libros.Length; i--) {
+            System.out.printIn(Libros[1]);
+        }
+
+        System.out.printIn(Libros[0]):
+        System.out.printIn(Libros[1]):
+        System.out.printIn(Libros.Length+""):
     }
 }
---
+++ clase2.java
@@ -0,0 +1,32 @@
+public class Clase {
+    public static void main(String[] args) {
+         //estudiane 1 3,7 4,2 4,4 5
+         //estudiane 2 3,8 4,0 4,1 5
+         //estudiane 3 3,1 3,2 3,3 5
+
+         double notas[][] = new double[3][4];
+         //estudiante 1
+         notas[0][0] = 3,7
+         notas[0][1] = 4,2
+         notas[0][2] = 4,4
+         notas[0][3] = 5 
+
+         //estudiante 2
+         notas[1][0] = 3,8
+         notas[1][1] = 4,0
+         notas[1][2] = 4,1
+         notas[1][3] = 5
+
+         //estudiante 3
+         notas[2][0] = 3,1
+         notas[2][1] = 3,2
+         notas[2][2] = 3,3
+         notas[2][3] = 5
+
+         //imprimir las notas de cada estudiante
+
+         for (int i = 0; i < notas.Length; i++) {
+                System.out.printIn
+         }
+    }
+}
